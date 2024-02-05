03a3ccf15a4fe6edc2b95fa986efbd04c448c161
Egor <egor.shelemetev228@mail.ru>
2024-02-05 14:57:39 +1000

    Доделал NavLink в компоненте Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,15 @@
 import React from "react"
 import s from './Login.module.css'
+import {NavLink} from 'react-router-dom'
+
+let setActive = ({ isActive }) => (isActive ? s['login__btn--active'] : s['login__btn'])
 
 const Login = React.memo(() => {
     return(
         <div className={s.login}>
-            <div className={s.login__btns}>
-                <div className={s['login__btn']}>
-                    <p className={s['login__btn-text']}>LOGIN</p>
-                </div>
-                <div className={s['login__btn']}>
-                    <p className={s['login__btn-text']}>REGISTER</p>
-                </div>
+            <div className={s.login__btns}>        
+                <NavLink to={'/'} className={setActive}>LOGIN</NavLink>
+                <NavLink to={'/refister'} className={setActive}>REGISTER</NavLink>
             </div>
         </div>
     )

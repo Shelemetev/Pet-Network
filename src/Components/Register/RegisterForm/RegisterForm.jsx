ab613f1f966475b2d99e84ba3ff99411f58d2cca
Egor <egor.shelemetev228@mail.ru>
2024-02-16 14:23:59 +1000

    Сделал валидацию для регистрации

--- src/Components/Register/RegisterForm/RegisterForm.jsx
+++ src/Components/Register/RegisterForm/RegisterForm.jsx
@@ -30,7 +30,27 @@ const RegisterForm = React.memo(({registredThunk,registerStatus,startLoader}) =>
     }
 
     return (
-        <Formik initialValues={initialValues} onSubmit={registerStart}>
+        <Formik initialValues={initialValues} onSubmit={registerStart}
+            validate={values => {
+                const errors = {}
+                if (!values.username) {
+                    errors.username = "Enter the user name"
+                } 
+                if (!values.firstName) {
+                    errors.firstName = "Enter the firstname" 
+                }
+                if (!values.lastName) {
+                    errors.lastName = "Enter the lastName"
+                }
+                if (!values.email) {
+                    errors.email = "Enter the email"
+                }
+                if (!values.password) {
+                    errors.password = "Enter the password"
+                }
+                return errors
+            }}
+        >
             {({
             values,
             errors,
@@ -46,29 +66,47 @@ const RegisterForm = React.memo(({registredThunk,registerStatus,startLoader}) =>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="username" onChange={handleChange} value={values.username} onBlur={handleBlur} /> 
                     </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}>{errors.username}</p>
+                    </div>
                     <p className={s.form__text}>firstName</p>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="firstName" onChange={handleChange} value={values.firstName} onBlur={handleBlur} />
+                    </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}>{errors.firstName}</p>
                     </div>
                     <p className={s.form__text}>lastName</p>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="lastName" onChange={handleChange} value={values.lastName} onBlur={handleBlur} /> 
                     </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}>{errors.lastName}</p>
+                    </div>
                     <p className={s.form__text}>email</p>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="email" onChange={handleChange} value={values.email} onBlur={handleBlur} /> 
+                    </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}>{errors.email}</p>
                     </div>
                     <p className={s.form__text}>password</p>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="password" onChange={handleChange} value={values.password} onBlur={handleBlur} /> 
                     </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}>{errors.password}</p>
+                    </div>
                     <p className={s.form__text}>phone</p>
                     <div className={s['form__input-box']}>
                         <input className={s.form__input} type="text" name="phone" onChange={handleChange} value={values.phone} onBlur={handleBlur} /> 
                     </div>
+                    <div className={s.error__box}>
+                        <p className={s.error__text}></p>
+                    </div>
                     <button className={s.form__btn} type="submit" disabled={isSubmitting}>Register!</button>
                 </form >    
-            )}
+            )} 
         </Formik>
     )
 })
